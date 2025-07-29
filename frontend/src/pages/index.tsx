b6139cce06f0f8ce94c24dca12563ced540f2596
Fene-87 <Fenekayas87@gmail.com>
2025-07-29 17:38:15 +0300

    testing 404

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,22 +1,31 @@
-import { useState } from 'react'
-import InputBox from '../components/InputBox'
-import ResponseCard from '../components/ResponseCard'
-import LoadingState from '../components/LoadingState'
-import Footer from '@/components/Footer';
-import Header from '@/components/Header';
+// import { useState } from 'react'
+// import InputBox from '../components/InputBox'
+// import ResponseCard from '../components/ResponseCard'
+// import LoadingState from '../components/LoadingState'
+// import Footer from '@/components/Footer';
+// import Header from '@/components/Header';
+
+// export default function Home() {
+//   const [response, setResponse] = useState('')
+//   const [loading, setLoading] = useState(false)
+
+//   return (
+//     <div className="flex flex-col min-h-screen">
+//       <Header />
+//       <main className="flex-1 p-4">
+//         <InputBox setResponse={setResponse} setLoading={setLoading} />
+//         {loading ? <LoadingState /> : <ResponseCard content={response} />}
+//       </main>
+//       <Footer />
+//     </div>
+//   )
+// }
 
 export default function Home() {
-  const [response, setResponse] = useState('')
-  const [loading, setLoading] = useState(false)
-
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <main className="flex-1 p-4">
-        <InputBox setResponse={setResponse} setLoading={setLoading} />
-        {loading ? <LoadingState /> : <ResponseCard content={response} />}
-      </main>
-      <Footer />
+    <div>
+      <h1>Hello World - Test</h1>
+      <p>If you see this, routing works!</p>
     </div>
-  )
+  );
 }
